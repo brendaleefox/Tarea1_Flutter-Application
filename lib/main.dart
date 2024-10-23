1d31942343c508c638d6cb73d4aa68729cdd80fd
Brenda Fox <brendafox@unitec.edu>
2024-10-22 23:37:18 -0600

    Tercer Commit: Haz que la app sea mas atractiva

--- lib/main.dart
+++ lib/main.dart
@@ -38,21 +38,57 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;  
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random idea for today:'),
-          Text(appState.current.asLowerCase),
-          
-          ElevatedButton(
-            onPressed: () {
-              //print('button pressed!');
-              appState.getNext(); 
-            },
-            child: Text('Next'),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //Text('A random idea for today:'),
+            //Text(appState.current.asLowerCase),
+            BigCard(pair: pair), 
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                //print('button pressed!');
+                appState.getNext(); 
+              },
+              child: Text('Next'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,  
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        //child: Text(pair.asLowerCase),
+        //child: Text(pair.asLowerCase, style: style),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
